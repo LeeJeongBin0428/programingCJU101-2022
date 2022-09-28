4fe84d499166f14768475cd9cc7d0bbf6d4f7252
LeeJeongBin0428 <jengbin@naver.com>
2022-09-28 12:36:22 +0900

    3주차 3번문제
    .

---
+++ 3주차 3번문제/3번문제.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double hight 179.5;
+	double weight 75.0;
+
+	if ((hight >= 187.5) && (weight < 80))
+	{
+		printf("%ok\n");
+
+
+	}
+
+	else{
+
+		printf("%cancle\n");
+
+	
+	}
+
+	return 0;
+
+
+
+
+}
