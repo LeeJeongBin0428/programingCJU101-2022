807f29426c648db66dfa047f3b686968cbcf33b3
Jeong Bin lee <jengbin@naver.com>
2022-10-05 11:49:22 +0900

    사칙연산프로그램
    완료

---
+++ 사칙연상프로그램/소스.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 20, b = 3;
+
+	printf("%d+%d = %d", a, b, a + b);
+	printf("%d-%d = %d", a, b, a - b);
+	printf("%d*%d = %d", a, b, a * b);
+	printf("%d/%d = %d", a, b, a / b); 
+	printf("%d %% %d = %d", a, b, a % b);
+
+
+
+
+
+}
