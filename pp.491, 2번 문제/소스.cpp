55ef6c5b505e0c0ea739e3e73d6e49baadff045b
LeeJeongBin0428 <112993970+LeeJeongBin0428@users.noreply.github.com>
2022-11-16 12:48:31 +0900

    Add files via upload

---
+++ pp.491, 2번 문제/소스.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+{
+struct cracker {
+	int price;
+	int calories;
+};
+int main()
+{
+	struct carcker zc;
+	printf("바사삭의 가격과 열량을 입력하시오:");
+	scanf_s("%d%d", &zc.price,&zc.calories);
+		printf("바사삭의 가격: %d원\n", zc.price);
+		printf("바사삭의 열량: %dkcal\n", zc.calories);
+		
+		
+		return 0;
+
+
+}
